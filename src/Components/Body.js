ac7baf6d7f089e326abcd1128e24c76f0864a91d
ruchim24 <ruchimajoka24@gmail.com>
2024-05-30 01:10:49 +0530

    added food cards

---
+++ src/Components/Body.js
@@ -0,0 +1,18 @@
+import React from "react";
+import RestrautantCard from "./RestraurantCard";
+import resList from '../../utils/mockData';
+
+const Body = () => {
+    return (
+      <div className="body">
+        <div className="search">Search</div>
+        <div className="res-container">
+          {resList?.map((res) => {
+            return <RestrautantCard key={res.info.id} resData={res.info} />;
+          })}
+        </div>
+      </div>
+    );
+  };
+
+  export default Body;
---
+++ src/Components/RestraurantCard.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { CDN_URL } from "../../utils/constants";
+
+const RestrautantCard = (props) => {
+    const { name, cloudinaryImageId, costForTwo, cuisines, avgRating, sla } =
+      props.resData;
+    return (
+      <div className="res-card">
+        <img
+          className="res-img"
+          alt="res-logo"
+          src={CDN_URL+cloudinaryImageId}
+        />
+        <h3>{name}</h3>
+        <h4>{cuisines.join(",")}</h4>
+        <h4>{costForTwo}</h4>
+        <h4>{avgRating} star rating</h4>
+        <h4>{sla.deliveryTime} mins</h4>
+      </div>
+    );
+  };
+
+  export default RestrautantCard;
